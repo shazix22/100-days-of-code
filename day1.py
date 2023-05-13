b8d1044b6d19c95f4af3517d7df44759f0b7b855
shazix22 <107991373+shazix22@users.noreply.github.com>
2023-05-13 17:30:43 +1000

    Create day1.py

---
+++ day1.py
@@ -0,0 +1,8 @@
+print("Welcome To The Bnad Name Generator!")
+print("What is the name of the city you grew up in?")
+x = input()
+print("How did your first pet die?")
+y = input()
+final = x + y
+print("Your bnad name could be " ,final)
+print("Also, you're a terrible person ;)")
